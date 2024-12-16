62737dee03fc946f4dd393b05dbec5bf5245e205
Evlen Fryling <evlen.fryling@gmail.com>
2024-12-16 16:58:39 -0500

    First commit after part one

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const yello = 1;
+yello =2;
+console.log (yello);
+let bird = "bauldar";
+bird ="Frey";
+console.log(bird)
+*/
